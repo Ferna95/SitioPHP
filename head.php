65257c7d74cf381fa8922eb74c2eff201ecf1e9b
Ferna95 <percha_biker@hotmail.com>
2018-01-29 19:39:07 -0300

    Cambio de nombre de hoja de estilo

--- head.php
+++ head.php
@@ -14,7 +14,7 @@
         <title>Simple example about CSS3 animations</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/demo.css" rel="stylesheet" type="text/css" />
+        <link href="css/estilo.css" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     </head>
     <body>
