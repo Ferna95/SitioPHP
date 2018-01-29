e6362df73f449fdb743280cad3bf11249aceeb2a
Ferna95 <percha_biker@hotmail.com>
2018-01-29 19:21:38 -0300

    Agregado de cuadros en pagina principal

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         <title>Simple example about CSS3 animations</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/demo.css" rel="stylesheet" type="text/css" />
+        <link href="css/estilo.css" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     </head>
     <body>
@@ -67,15 +67,34 @@
         		<img class="img1" src="https://lh3.googleusercontent.com/8w3cPZkDLEmEK0JKZIkbEB-QBLuQORU6q_NkBiDGBoGCaHdsEqYHlYy8UTiQq11mEQ=h900">
         		<img class="img2" src="https://hdwallsource.com/img/2014/6/wallpapers-hd-7974-8304-hd-wallpapers.jpg">
         		<img class="img3" src="https://hdwallsource.com/img/2014/2/cool-wallpapers-hd-8087-8418-hd-wallpapers.jpg">
+
+                <nav class="nav-radio">
+                    <input type="radio" id="cb" class="c1" name="cb" checked="true"/>
+                    <input type="radio" id="cb" class="c2" name="cb" />
+                    <input type="radio" id="cb" class="c3" name="cb" />
+                </nav>
         	</div>
 
-        	<nav class="nav-radio">
-        		<input type="radio" id="cb" class="c1" name="cb" checked="true"/>
-        		<input type="radio" id="cb" class="c2" name="cb" />
-        		<input type="radio" id="cb" class="c3" name="cb" />
-        	</span>
-
         	
+
+        	<div class="news">
+                <div class="new">
+                    <div class="content-new">
+                        <img class="img-new" src="" />
+                        <h2 class="title-new">Todas las rutinas</h2>
+                        <p class="p-new">Consultá con nuestros profesionales todas las rutinas
+                        que tenemos para vos según tus objetivos ¡Ellos están para ayudarte!</p>
+                    </div>
+                </div>
+                <div class="new">
+                    <div class="content-new">
+                        <img class="img-new" src="" />
+                        <h2 class="title-new">Clases todos los días</h2>
+                        <p class="p-new">Sumate a las clases personalizadas que dictamos 
+                        todos los días, siempre con el apoyo de nuestros profesionales ¡Recordá inscribirte para asistir!</p>
+                    </div>
+                </div>
+            </div>
 
 
 
