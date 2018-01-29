10308db908b58a1b67a8484657d56c1bca1779c4
Ferna95 <percha_biker@hotmail.com>
2018-01-29 20:23:31 -0300

    Agregado de estilo de lineas de tabla de clases

--- inscripcion.php
+++ inscripcion.php
@@ -1,32 +1,31 @@
 <?php include("head.php") ?>
-
-	<div class="calendario">
-		<h1>Clases semanales</h1>
-		<table class="calendar-table">
-			<th>
-				<td></td>
-				<td>Lunes</td>
-				<td>Martes</td>
-				<td>Miércoles</td>
-				<td>Jueves</td>
-				<td>Viernes</td>
-				<td>Sábado</td>
-				<td>Domingo</td>
-			</th>
-			<?php for($i=7;$i<=22;$i++){ ?>
+	<div class="contenedor">
+		<div class="calendario">
+			<h1>Clases semanales</h1>
+			<table class="calendar-table">
 				<tr>
-					<?php for($j=0;$j<=7;$j++){ ?>
-						<td><?php echo "$i" . ":00 - " . ($i+1) .":00"?></td>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td></td>
+					<td></td>
+					<td>Lunes</td>
+					<td>Martes</td>
+					<td>Miércoles</td>
+					<td>Jueves</td>
+					<td>Viernes</td>
+					<td>Sábado</td>
+					<td>Domingo</td>
+				</tr>
+				<?php for($i=7;$i<=22;$i++){ ?>
+					<?php if($i%2==0){ ?>
+						<tr class="tr2">
+					<?php }else{ ?>
+						<tr class="tr1">
 					<?php } ?>
-				</tr>	
-			<?php } ?>
-		</table>
+							<td><?php echo "$i" . ":00 - " . ($i+1) .":00" ;?></td>
+							<?php for($j=1;$j<=7;$j++){ ?>
+								<td><?php echo ("$i and $j"); ?></td>
+							<?php } ?>
+						</tr>	
+				<?php } ?>
+			</table>
+		</div>
 	</div>
 <?php include("foot.php") ?>
